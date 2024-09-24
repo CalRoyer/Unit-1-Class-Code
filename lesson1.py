4f9c759e10260676659e38f246424ab5f6abb6ee
royerc_wlhs <royerc@wlhs.wlwv.k12.or.us>
2024-09-24 09:49:55 -0700

    new file made

---
+++ lesson1.py
@@ -0,0 +1,67 @@
+'''
+Cal Royer
+9-16-24
+Unit 1 Lesson Notes
+
+'''
+
+# Variables
+
+message = "Hello, user"
+print(message)
+# snake_case
+user_name = input("Enter your name ")
+user_id = int(input("Enter your id: "))
+
+#Variable type
+#print(type(user_name))
+
+# Strings
+# can use' or " to indicate string - be consistent
+
+# f-strings are formatted strings that help with combining string
+
+# Way 1 to combine string: use +
+# Caution: all numbers have str( ) around them
+message_one = "Welcome " + user_name + " with ID " + str(user_id)
+print(message)
+
+# Way 2 - use f strings
+message_two = f"Welcome {user_name} with ID {user_id}"
+print(message_two)
+
+# Possible errors
+famous_quotation =
+
+# raw strings
+whale = r""" 
+      .
+      ":"
+    ___:____     |"\/"|
+  ,'        `.    \  /
+  |  O        \___/  |
+~^~^~^~^~^~^~^~^~^~^~^~^~
+"""
+print(whale)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
